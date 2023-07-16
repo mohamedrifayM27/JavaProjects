2e3e7305aa9f75770b8b15feb213a7c74c7adbd6
Mohamed Rifay <80139074+mohamedrifayM27@users.noreply.github.com>
2023-07-16 21:07:24 +0530

    Add files via upload

---
+++ BankingApplication/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BankAccount customer1 = new BankAccount("Rahul","ABC123");
+        customer1.show();
+        BankAccount customer2 = new BankAccount("Raju","BBC198");
+        customer2.show();
+    }
+}
