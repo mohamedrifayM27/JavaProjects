5759e3ebc33135687a0bb263f84451d20105155a
Mohamed Rifay <80139074+mohamedrifayM27@users.noreply.github.com>
2023-07-16 20:43:10 +0530

    Delete BankingApplication directory

--- BankingApplication/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BankAccount customer1 = new BankAccount("Rahul","ABC123");
-        customer1.show();
-        BankAccount customer2 = new BankAccount("Raju","BBC198");
-        customer2.show();
-    }
-}
